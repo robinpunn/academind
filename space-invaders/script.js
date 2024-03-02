0f717a5980209d7da39e045e24aa29e153113b83
Robin Punnoose <robin.punnoose@protonmail.com>
2024-03-02 17:58:13 -0600

    updated Player class passing game to constructor and adding draw/update method. updated window event listener with animate function

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -1,22 +1,58 @@
+// we don't really need a class here since there will only be 1 player
 class Player {
-
+    constructor(game) {
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
+        this.speed = 0;
+    }
+    draw(context) {
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+        this.x += this.speed;
+    }
 }
 
+// class that will handle various types of projectiles
 class Projectile {
 
 }
 
+// class that will handle various types of enemies 
 class Enemy {
 
 }
 
+// contains the main logic for the entire game
 class Game {
-
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.player = new Player(this);
+    }
+    render(context) {
+        this.player.draw(context);
+        this.player.update();
+    }
 }
 
+// ensures js runs only after all our art assets are loaded
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 600;
     canvas.height = 800;
+
+    const game = new Game(canvas);
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        window.requestAnimationFrame(animate);
+    }
+    animate();
 })
