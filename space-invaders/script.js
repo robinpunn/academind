eab0788c9d1754b498cb5de358e703dc1eed791e
Robin Punnoose <robin.punnoose@protonmail.com>
2024-03-04 15:50:42 -0600

    updated projectile class, player can move out of bounds on left x axis so player can shoot enemies on the edge of the map

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -8,16 +8,18 @@ class Player {
         this.y = this.game.height - this.height;
         this.speed = 5;
     }
+    // create the player
     draw(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
     }
+    // player movement
     update() {
         // horizontal movement
         if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
         if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
         // horizontal boundaries
-        if (this.x < 0) this.x = 0;
-        else if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        if (this.x < 0 - this.width * 0.5) this.x = - this.width * 0.5;
+        else if (this.x > this.game.width - this.width * 0.5) this.x = this.game.width - this.width * 0.5;
         // vertical movement
         if (this.game.keys.indexOf('ArrowUp') > -1) this.y -= this.speed;
         if (this.game.keys.indexOf('ArrowDown') > -1) this.y += this.speed;
@@ -25,11 +27,46 @@ class Player {
         if (this.y < 0) this.y = 0;
         else if (this.y > this.game.height - this.height) this.y = this.game.height - this.height;
     }
+    // shoot projectile
+    shoot() {
+        const projectile = this.game.getProjectile();
+        if (projectile) projectile.start(this.x + this.width * 0.5, this.y);
+    }
 }
 
 // class that will handle various types of projectiles
 class Projectile {
-
+    constructor() {
+        this.width = 8;
+        this.height = 40;
+        this.x = 0;
+        this.y = 0;
+        this.speed = 20;
+        this.free = true;
+    }
+    // create projectiles as long as this.free is not true
+    draw(context) {
+        if (!this.free) {
+            context.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+    // movement for projectiles
+    update() {
+        if (!this.free) {
+            this.y -= this.speed;
+            if (this.y < -this.height) this.reset();
+        }
+    }
+    // runs when object is taken from object pool
+    start(x, y) {
+        this.x = x - this.width * 0.5;
+        this.y = y;
+        this.free = false
+    }
+    // runs when object is no longer needed
+    reset() {
+        this.free = true;
+    }
 }
 
 // class that will handle various types of enemies 
@@ -46,10 +83,16 @@ class Game {
         this.keys = [];
         this.player = new Player(this);
 
+        this.projectilesPool = [];
+        this.numberOfProjectiles = 10;
+        this.createProjectiles();
+        console.log(this.projectilesPool);
+
         // event listeners
         window.addEventListener('keydown', e => {
             if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
-            // console.log(this.keys)
+            // console.log(e.key, this.keys)
+            if (e.key === "q") this.player.shoot();
         })
 
         window.addEventListener('keyup', e => {
@@ -58,9 +101,26 @@ class Game {
             // console.log(this.keys)
         })
     }
+    // render objects onto the canvas
     render(context) {
         this.player.draw(context);
         this.player.update();
+        this.projectilesPool.forEach(projectile => {
+            projectile.update();
+            projectile.draw(context);
+        })
+    }
+    // create projectiles object pool
+    createProjectiles() {
+        for (let i = 0; i < this.numberOfProjectiles; i++) {
+            this.projectilesPool.push(new Projectile());
+        }
+    }
+    // get free projectile object from the pool
+    getProjectile() {
+        for (let i = 0; i < this.projectilesPool.length; i++) {
+            if (this.projectilesPool[i].free) return this.projectilesPool[i];
+        }
     }
 }
 
