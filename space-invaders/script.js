8f19f2cfa45da791b4a9f7a7982d414c62c8ff8e
Robin Punnoose <robin.punnoose@protonmail.com>
2024-03-03 20:23:16 -0600

    added player movement and game boundries

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -6,13 +6,24 @@ class Player {
         this.height = 100;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
-        this.speed = 0;
+        this.speed = 5;
     }
     draw(context) {
         context.fillRect(this.x, this.y, this.width, this.height);
     }
     update() {
-        this.x += this.speed;
+        // horizontal movement
+        if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+        if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+        // horizontal boundaries
+        if (this.x < 0) this.x = 0;
+        else if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        // vertical movement
+        if (this.game.keys.indexOf('ArrowUp') > -1) this.y -= this.speed;
+        if (this.game.keys.indexOf('ArrowDown') > -1) this.y += this.speed;
+        // vertical boundaries
+        if (this.y < 0) this.y = 0;
+        else if (this.y > this.game.height - this.height) this.y = this.game.height - this.height;
     }
 }
 
@@ -32,7 +43,20 @@ class Game {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.keys = [];
         this.player = new Player(this);
+
+        // event listeners
+        window.addEventListener('keydown', e => {
+            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+            // console.log(this.keys)
+        })
+
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key);
+            if (index > -1) this.keys.splice(index, 1);
+            // console.log(this.keys)
+        })
     }
     render(context) {
         this.player.draw(context);
